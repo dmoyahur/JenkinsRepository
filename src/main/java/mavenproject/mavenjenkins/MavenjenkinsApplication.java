3b0a986bcd816a2b2254e1f54f6ea38b86a1726b
Diego Moya <dm.moya.ext@orangebank.es>
2024-05-03 03:33:34 +0200

    Add maven project source code

---
+++ src/main/java/mavenproject/mavenjenkins/MavenjenkinsApplication.java
@@ -0,0 +1,13 @@
+package mavenproject.mavenjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenjenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenjenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mavenproject/mavenjenkins/MavenjenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package mavenproject.mavenjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenjenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
